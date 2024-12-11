732cfb7a78b6a627012fabe19459370eed1c7891
Hatim <hatim.slimani.19@gmail.com>
2024-12-11 17:41:00 +0100

    Test-First the POST method
    - Writing a test of what success will look like.
    - Now, the test will fail.

--- src/test/java/com/cashcard/CashCardApplication/CashCardApplicationTests.java
+++ src/test/java/com/cashcard/CashCardApplication/CashCardApplicationTests.java
@@ -36,4 +36,12 @@ class CashCardApplicationTests {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isBlank();
     }
+
+    @Test
+    void shouldCreateANewCashCard(){
+        CashCard cashCard = new CashCard(null, 250.0);
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", cashCard, Void.class);
+
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 }
